35bf2866152d20d622dae031a1ca9b1b67fac9d4
Toymaker <toycraftworld@gmail.com>
2025-01-19 18:00:21 +0900

    Update FilePlayer.py

--- FilePlayer.py
+++ FilePlayer.py
@@ -1,24 +1,25 @@
 import keyboard
 import pyautogui
+import time
 
 class FilePlayer:
     def __init__(self):
         self.m_mappings = {}
 
     def register(self, a_key, a_file_name):
-        with open(a_file_name, 'r') as file:
-            content = file.read()
-        self.m_mappings[a_key] = (content)
+        self.m_mappings[a_key] = a_file_name
         keyboard.add_hotkey(a_key, lambda: self.type_content(a_key))
 
     def type_content(self, a_key):
-        content = self.m_mappings[a_key]
+        file_name = self.m_mappings[a_key]
+        with open(file_name, 'r') as file:
+            content = file.read()
         for char in content:
+            if ' ' == char:
+                time.sleep(0.1)
+            if '\n' == char:
+                time.sleep(0.3)
             pyautogui.typewrite(char, interval=0.0001)
-
-        #words = content.split()
-        #for word in words:
-        #    pyautogui.typewrite(word + ' ', interval=0.01)
 
     def play(self):
         print("Press 'esc' to exit.")
